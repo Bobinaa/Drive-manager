60468c251c7aa9d32ad6324dff48e6f93eecfb76
Maxim <kitakk17@gmail.com>
2018-10-28 15:50:07 +0500

    Init

--- DVD open/Form1.Designer.cs
+++ DVD open/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,90 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cDrives = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // cDrives
+            // 
+            this.cDrives.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cDrives.FormattingEnabled = true;
+            this.cDrives.Location = new System.Drawing.Point(12, 16);
+            this.cDrives.Name = "cDrives";
+            this.cDrives.Size = new System.Drawing.Size(183, 21);
+            this.cDrives.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(201, 16);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(92, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Открыть ";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(201, 43);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(92, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 48);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(60, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Вконтакте";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(305, 78);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cDrives);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Управление дисководом";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cDrives;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+
     }
 }
 
--- DVD open/Form1.cs
+++ DVD open/Form1.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace DVD_open
 {
@@ -15,6 +15,71 @@ namespace DVD_open
         public Form1()
         {
             InitializeComponent();
+
+            //Заполнение списка дисковых накопителей
+            getDrives(ref cDrives);
+        }
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendString")]
+        public static extern int mciSendStringA(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        private void getDrives(ref ComboBox cList)
+        {
+            //Очистить список
+            cList.Items.Clear();
+
+            //Добавить только CD-диски
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == DriveType.CDRom)
+                    cList.Items.Add(drive.RootDirectory);
+            }
+        }
+
+        private void openDrive(string driveRoot)
+        {
+            string driveLetter = driveRoot[0].ToString();
+            string returnString = string.Empty;
+
+            //Задаём имя для указанного диска
+            mciSendStringA("open " + driveLetter + ": type CDaudio alias drive" + driveLetter, returnString, 0, 0);
+
+            //Открыть лоток для диска
+            mciSendStringA("set drive" + driveLetter + " door open", returnString, 0, 0);
+        }
+
+        private void closeDrive(string driveRoot)
+        {
+            string driveLetter = driveRoot[0].ToString();
+            string returnString = string.Empty;
+
+            //Задаём имя для указанного диска
+            mciSendStringA("open " + driveLetter + ": type CDaudio alias drive" + driveLetter, returnString, 0, 0);
+
+            //Закрываем лоток для диска
+            mciSendStringA("set drive" + driveLetter + " door closed", returnString, 0, 0);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (cDrives.Text != string.Empty)
+                openDrive(cDrives.Text);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (cDrives.Text != string.Empty)
+                closeDrive(cDrives.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://vk.com/jobbed");
         }
     }
 }
